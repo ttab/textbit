4cd9664d52ce4ab2873de738e041aaa5ba4cd1af
Danne Lundqvist <danne@dotvoid.com>
2023-08-30 16:40:53 +0200

    Improved simple rendering test

--- src/components/editor/editor.test.tsx
+++ src/components/editor/editor.test.tsx
@@ -8,7 +8,7 @@ describe("MimerEditor", () => {
     const initialValue: Descendant[] = [
         {
             type: 'core/heading-1',
-            id: '538345e5-bacc-48f9-8ef1-a219891b60eb',
+            id: 'be0ec554-839d-413c-9140-c408cb213f1e',
             class: 'text',
             children: [
                 { text: 'This is title' }
@@ -16,14 +16,18 @@ describe("MimerEditor", () => {
         },
         {
             type: 'core/paragraph',
+            class: 'text',
+            id: 'fc542b22-6046-49d8-8eae-56a8597599a3',
             children: [{ text: 'This is paragraph' }],
         }
     ]
 
     test("renders the MimerEditor component", () => {
-        render(<MimerEditor value={initialValue} onChange={() => { return }} />)
+        const { unmount, container } = render(<MimerEditor value={initialValue} onChange={() => { return }} />)
 
         expect(screen.getByRole('textbox'))
+        expect(container.querySelectorAll('div[data-id="be0ec554-839d-413c-9140-c408cb213f1e"]')).toHaveLength(2)
+        expect(container.querySelectorAll('div[data-id="fc542b22-6046-49d8-8eae-56a8597599a3"]')).toHaveLength(2)
         expect(screen.getByText('This is title'))
         expect(screen.getByText('This is paragraph'))
     })
