79ae1107aa0aa5ace82f243bcdfbd34f71ecdf65
Danne Lundqvist <danne@dotvoid.com>
2023-10-25 11:37:41 +0200

    Fixed import name and whitespace.

--- local/src/index.tsx
+++ local/src/index.tsx
@@ -1,114 +1,114 @@
 import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Editor as TextbitEditor } from '../../src'
+import { TextbitEditor } from '../../src'
 import { ThemeSwitcher } from './themeSwitcher'
 import { Descendant } from 'slate'
 
 const initialValue: Descendant[] = [
-    {
-        type: 'core/text',
-        id: '538345e5-bacc-48f9-8ef1-a219891b60eb',
+  {
+    type: 'core/text',
+    id: '538345e5-bacc-48f9-8ef1-a219891b60eb',
+    class: 'text',
+    properties: {
+      type: 'h1'
+    },
+    children: [
+      { text: 'Better music?' }
+    ]
+  },
+  {
+    type: 'core/text',
+    id: '538345e5-bacc-48f9-9ed2-b219892b51dc',
+    class: 'text',
+    properties: {
+      type: 'preamble'
+    },
+    children: [
+      { text: 'It is one of those days when better music makes all the difference in the world. At least to me, my inner and imaginary friend.' }
+    ]
+  },
+  {
+    type: 'core/text',
+    id: '538345e5-cadd-4558-9ed2-a219892b51dc',
+    class: 'text',
+    properties: {
+      type: 'dateline'
+    },
+    children: [
+      { text: 'Kalmar' }
+    ]
+  },
+  {
+    type: 'core/text',
+    id: '538345e5-bacc-48f9-8ef0-1219891b60ef',
+    class: 'text',
+    children: [
+      { text: 'An example paragraph that contains text that is a wee bit ' },
+      { text: 'stronger', formats: ['core/bold'] },
+      { text: ' than normal but also text that is somewhat ' },
+      { text: 'emphasized', formats: ['core/italic'] },
+      { text: ' compared to the normal styled text found elsewhere in the document.' },
+    ],
+  },
+  {
+    type: 'core/blockquote',
+    class: 'textblock',
+    id: '538345e5-bacc-48f9-8ef1-1214443a32da',
+    children: [
+      {
+        type: 'core/blockquote/body',
         class: 'text',
-        properties: {
-            type: 'h1'
-        },
         children: [
-            { text: 'Better music?' }
+          { text: 'Just a regular paragraph that contains some nonsensical writing' }
         ]
-    },
-    {
-        type: 'core/text',
-        id: '538345e5-bacc-48f9-9ed2-b219892b51dc',
+      },
+      {
+        type: 'core/blockquote/caption',
         class: 'text',
-        properties: {
-            type: 'preamble'
-        },
         children: [
-            { text: 'It is one of those days when better music makes all the difference in the world. At least to me, my inner and imaginary friend.' }
+          { text: 'Mr Smith' }
         ]
-    },
-    {
-        type: 'core/text',
-        id: '538345e5-cadd-4558-9ed2-a219892b51dc',
-        class: 'text',
-        properties: {
-            type: 'dateline'
-        },
-        children: [
-            { text: 'Kalmar' }
-        ]
-    },
-    {
-        type: 'core/text',
-        id: '538345e5-bacc-48f9-8ef0-1219891b60ef',
-        class: 'text',
-        children: [
-            { text: 'An example paragraph that contains text that is a wee bit ' },
-            { text: 'stronger', formats: ['core/bold'] },
-            { text: ' than normal but also text that is somewhat ' },
-            { text: 'emphasized', formats: ['core/italic'] },
-            { text: ' compared to the normal styled text found elsewhere in the document.' },
-        ],
-    },
-    {
-        type: 'core/blockquote',
-        class: 'textblock',
-        id: '538345e5-bacc-48f9-8ef1-1214443a32da',
-        children: [
-            {
-                type: 'core/blockquote/body',
-                class: 'text',
-                children: [
-                    { text: 'Just a regular paragraph that contains some nonsensical writing' }
-                ]
-            },
-            {
-                type: 'core/blockquote/caption',
-                class: 'text',
-                children: [
-                    { text: 'Mr Smith' }
-                ]
-            }
-        ]
-    },
-    {
-        type: 'core/text',
-        class: 'text',
-        id: '538345e5-bacc-48f9-8ef1-1215892b61ed',
-        children: [
-            { text: 'This, here now is just a regular paragraph that contains some nonsensical writing written by me.' },
-        ],
-    },
-    {
-        type: 'core/text',
-        id: '538343b5-badd-48f9-8ef0-1219891b60ef',
-        class: 'text',
-        children: [
-            { text: 'An example paragraph that contains text that is a wee bit ' },
-            { text: 'stronger', formats: ['core/bold'] },
-            { text: ' than normal but also text that is somewhat ' },
-            { text: 'emphasized', formats: ['core/italic'] },
-            { text: ' compared to the normal styled text found elsewhere in the document.' },
-        ],
-    }
+      }
+    ]
+  },
+  {
+    type: 'core/text',
+    class: 'text',
+    id: '538345e5-bacc-48f9-8ef1-1215892b61ed',
+    children: [
+      { text: 'This, here now is just a regular paragraph that contains some nonsensical writing written by me.' },
+    ],
+  },
+  {
+    type: 'core/text',
+    id: '538343b5-badd-48f9-8ef0-1219891b60ef',
+    class: 'text',
+    children: [
+      { text: 'An example paragraph that contains text that is a wee bit ' },
+      { text: 'stronger', formats: ['core/bold'] },
+      { text: ' than normal but also text that is somewhat ' },
+      { text: 'emphasized', formats: ['core/italic'] },
+      { text: ' compared to the normal styled text found elsewhere in the document.' },
+    ],
+  }
 ]
 
 function App() {
-    const [value, setValue] = useState<Descendant[]>(initialValue)
+  const [value, setValue] = useState<Descendant[]>(initialValue)
 
-    return (
-        <div style={{ position: 'relative', height: '1200px' }}>
-            <ThemeSwitcher />
+  return (
+    <div style={{ position: 'relative', height: '1200px' }}>
+      <ThemeSwitcher />
 
-            <TextbitEditor
-
-                value={initialValue}
-                onChange={value => {
-                    setValue(value)
-                }}
-            />
-        </div>
-    )
+      <TextbitEditor
+        value={initialValue}
+        onChange={value => {
+          setValue(value)
+        }}
+        verbose={true}
+      />
+    </div>
+  )
 }
 
 const container = document.getElementById('app')
