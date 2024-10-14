3cca81d56a97663e4c4b38caff40f86916f8047c
Danne Lundqvist <danne@dotvoid.com>
2024-10-14 16:26:15 +0200

    Recalculate menu content top margin when needed to show menu in window

--- src/components/ContentTools/Content.tsx
+++ src/components/ContentTools/Content.tsx
@@ -1,4 +1,10 @@
-import React, { PropsWithChildren, useContext, useEffect, useRef } from 'react'
+import React, {
+  PropsWithChildren,
+  useContext,
+  useLayoutEffect,
+  useRef,
+  useState
+} from 'react'
 import { MenuContext } from './Menu'
 import { useKeydownGlobal } from '@/hooks'
 import { createPortal } from 'react-dom'
@@ -7,7 +13,29 @@ export const Content = ({ children, className }: PropsWithChildren & {
   className?: string
 }) => {
   const [isOpen, setIsOpen] = useContext(MenuContext)
-  const ref = useRef<HTMLDivElement>(null)
+  const outerRef = useRef<HTMLDivElement>(null)
+  const innerRef = useRef<HTMLDivElement>(null)
+  const MIN_BOTTOM_MARGIN = 10
+
+  // Ensure sure the menu is not displayed below browser window bottom
+  useLayoutEffect(() => {
+    if (!innerRef?.current) {
+      return
+    }
+
+    let offset = 0
+    if (isOpen) {
+      const innerRect = innerRef.current.getBoundingClientRect()
+      const { paddingBottom, borderBottomWidth } = window.getComputedStyle(innerRef.current!)
+      const verticalSpace = parseFloat(paddingBottom) + parseFloat(borderBottomWidth)
+
+      if (innerRect.bottom > window.innerHeight - verticalSpace - MIN_BOTTOM_MARGIN) {
+        offset = Math.max(0, innerRect.height - (window.innerHeight - innerRect.top) + verticalSpace + MIN_BOTTOM_MARGIN)
+      }
+    }
+
+    innerRef.current.style.marginTop = `-${offset}px`
+  }, [isOpen, innerRef?.current])
 
   const keyTriggerRef = useKeydownGlobal<HTMLDivElement>((e) => {
     if (isOpen && (e.key === 'Escape' || e.key === 'Tab')) {
@@ -17,15 +45,13 @@ export const Content = ({ children, className }: PropsWithChildren & {
   })
 
   return <div ref={keyTriggerRef}>
-    <div ref={ref}>
-      {
-        isOpen && ref?.current && createPortal(
-          <div className={className}>
-            {children}
-          </div>,
-          ref.current
-        )
-      }
+    <div ref={outerRef}>
+      {isOpen && outerRef?.current && createPortal(
+        <div ref={innerRef} className={className}>
+          {children}
+        </div>,
+        outerRef.current
+      )}
     </div>
   </div>
 }
