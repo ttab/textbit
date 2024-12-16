17ded15ed1683dd88960b6bc05388d4b281cf0c6
Victor Lindh <victorlindh@hotmail.com>
2024-12-16 15:05:16 +0100

    conditionally remove div wrapper on child elements when rendering table node

--- lib/components/TextbitEditable/components/Element/ChildElement.tsx
+++ lib/components/TextbitEditable/components/Element/ChildElement.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Node } from 'slate'
 import type { RenderElementProps } from 'slate-react'
 import type { Plugin } from '../../../../types'
@@ -5,7 +6,7 @@ import type { Plugin } from '../../../../types'
 
 interface ChildElementProps extends RenderElementProps {
   entry: Plugin.ComponentEntry
-  rootNode: Node
+  rootNode: Node & { type?: string }
   options?: Record<string, unknown>
 }
 
@@ -16,6 +17,14 @@ interface ChildElementProps extends RenderElementProps {
  * @returns JSX.Element
  */
 export const ChildElement = ({ attributes, children, element, entry, rootNode, options }: ChildElementProps) => {
+  if (rootNode.type === 'core/table') {
+    return (
+      <React.Fragment {...attributes}>
+        {/* eslint-disable-next-line */}
+        {entry.component({ element, attributes, children, rootNode, options })}
+      </React.Fragment>
+  )
+  }
   return (
     <div className='child' data-id={element.id} {...attributes}>
       {/* eslint-disable-next-line */}
