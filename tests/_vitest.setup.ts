9190bfba7a92bee7262baf50c1b22861c5327b43
Danne Lundqvist <danne@dotvoid.com>
2025-11-11 15:20:40 +0100

    Add tests for spellcheck

--- tests/_vitest.setup.ts
+++ tests/_vitest.setup.ts
@@ -24,3 +24,76 @@ beforeAll(() => {
     }) as typeof structuredClone
   }
 })
+
+// Mock Range.getBoundingClientRect for jsdom
+beforeAll(() => {
+  Range.prototype.getBoundingClientRect = function() {
+    return {
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      toJSON: () => {}
+    }
+  }
+
+  Range.prototype.getClientRects = function() {
+    return {
+      length: 0,
+      item: () => null,
+      [Symbol.iterator]: function* () {}
+    } as DOMRectList
+  }
+})
+
+// Mock Element.getBoundingClientRect for better test support
+beforeAll(() => {
+  Element.prototype.getBoundingClientRect = function() {
+    return {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      top: 0,
+      left: 0,
+      right: 100,
+      bottom: 100,
+      toJSON: () => {}
+    }
+  }
+})
+
+// Mock window.getSelection
+beforeAll(() => {
+  window.getSelection = function() {
+    return {
+      rangeCount: 0,
+      addRange: () => {},
+      removeRange: () => {},
+      removeAllRanges: () => {},
+      getRangeAt: () => new Range(),
+      collapse: () => {},
+      collapseToEnd: () => {},
+      collapseToStart: () => {},
+      containsNode: () => false,
+      deleteFromDocument: () => {},
+      empty: () => {},
+      extend: () => {},
+      modify: () => {},
+      selectAllChildren: () => {},
+      setBaseAndExtent: () => {},
+      setPosition: () => {},
+      toString: () => '',
+      type: 'None',
+      anchorNode: null,
+      anchorOffset: 0,
+      focusNode: null,
+      focusOffset: 0,
+      isCollapsed: true
+    } as unknown as Selection
+  }
+})
